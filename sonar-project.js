8194440534d9d2ef795fcdab2d948a3fa2fcc670
Adwaithatruly <93597700+Adwaithatruly@users.noreply.github.com>
2021-12-15 21:06:36 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '17dd3a4a4ccc8874340c5d5baeb84dae63bd5c29',
+	    'sonar.login': '85ef39a05809bf09a86b20223a6061ae3f7f329b',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
